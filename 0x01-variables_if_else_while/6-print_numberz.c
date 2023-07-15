2058d9145ed4c411e6780ccc209294c8ebefb40b
“Far5uq” <faruqumar86@gmail.comgit config --global user.email faruqumar86@gmail.com>
2023-07-14 18:20:11 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: is 0
+ */
+
+int main(void)
+{
+int a;
+for (a = 0 ; a < 10 ; a++)
+putchar(a + '0');
+putchar("\n");
+return (0);
+}
