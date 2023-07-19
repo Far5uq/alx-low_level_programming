1d74f4c5faf5009a2b243e6f7d1e739b9617b945
“Far5uq” <faruqumar86@gmail.comgit config --global user.email faruqumar86@gmail.com>
2023-07-19 16:52:55 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,14 +4,14 @@
  * print_last_digit - function print last digits
  * @n: parameter to be printed
  *
- * Return 1 or 0
+ * Return: 1 or 0
  */
 
 int print_last_digit(int n)
 {
 int k;
 k = n % 10;
-if (n < 10)
+if (n < 0)
 k  = -k;
 _putchar(k + '0');
 return (k);
