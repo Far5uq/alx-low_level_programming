b93fd263dac1532ce2beaa28347d48735ce8a3cf
“Far5uq” <faruqumar86@gmail.comgit config --global user.email faruqumar86@gmail.com>
2023-07-27 08:14:11 -0700

    gert

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,11 +13,12 @@ char *_strcat(char *dest, char *src)
 int destlen = 0;
 int srclen = 0;
 int c;
-for (c = 0 ; dest[c] != '\0' ; c++)
+while (dest[c] != '\0')
 destlen++;
 for (c = 0 ; src[c] != '\0' ; c++)
-srclen++;
-for (c = 0 ; c <= srclen ; c++)
+{
 dest[destlen + c] = src[c];
+}
+dest[deslen + c] = '\0';
 return (dest);
 }
