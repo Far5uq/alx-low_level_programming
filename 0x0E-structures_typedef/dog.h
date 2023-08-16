01ddd5956c704c0f887401af37ebafd3d8e9daa0
“Far5uq” <faruqumar86@gmail.comgit config --global user.email faruqumar86@gmail.com>
2023-08-16 00:18:49 -0700

    nooooo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog dog_t;
+/**
+ * struct dog - structure of dog
+ * typedef struct - structure type
+ * init_dog -varriable type
+ * print_dog - structure
+ * *new_dog - prints new dog
+ * free_dog - frees dog
+ * @name: character
+ * @age: character
+ * @owner: character
+ *
+ * Return: Always 0.
+ */
 
 struct dog
 {
@@ -9,6 +21,7 @@ char *name;
 float age;
 char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
